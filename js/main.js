657aa36a31e951a681f1100cde5eb9763aa51336
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-03-16 17:49:45 +0100

    Add an event on createGridButton

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+const createGridButton = document.getElementById('create-grid');
 const container = document.getElementById('container');
 
 function createGrid(number) {
-    const gridDimension = parseInt(number) ** 2;
+    const gridDimension = number ** 2;
 
     for (let i = 0; i < gridDimension; i++) {
         let square = document.createElement('div');
@@ -14,4 +15,8 @@ function createGrid(number) {
     }
 }
 
-createGrid(prompt('Enter a dimension:'));
+createGridButton.addEventListener('click', () => {
+    const gridSize = Math.abs(parseInt(document.getElementById('grid-size').value) % 101) || 1;
+
+    createGrid(gridSize);
+});
