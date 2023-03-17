4ac72371ca6aaddb87a7dab9edabc723af09538d
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-03-17 16:45:44 +0100

    Add createSquare function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 const createGridButton = document.getElementById('create-grid');
 const container = document.getElementById('container');
 
+function createSquare() {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black'
+    });
+
+    container.appendChild(square);
+}
+
 function createGrid(number) {
     const gridDimension = number ** 2;
 
-    for (let i = 0; i < gridDimension; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'black'
-        });
-        
-        container.appendChild(square);
-    }
+    for (let i = 0; i < gridDimension; i++) createSquare();
 }
 
 createGridButton.addEventListener('click', () => {
