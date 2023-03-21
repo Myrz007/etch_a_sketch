19382276af3d63db42f813f7220bf6383dc1103e
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-03-21 15:03:16 +0100

    Edit createSquare function

--- js/main.js
+++ js/main.js
@@ -2,13 +2,8 @@ const createGridButton = document.getElementById('create-grid');
 const container = document.getElementById('container');
 
 function createSquare(number) {
-    const squareDimension = `${Math.floor(960 / number)}px`;
     let square = document.createElement('div');
-
     square.classList.add('square');
-    square.style.height = squareDimension;
-    square.style.width = squareDimension;
-    
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'black'
     });
